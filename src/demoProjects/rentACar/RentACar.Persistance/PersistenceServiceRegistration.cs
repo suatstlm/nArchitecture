55032d097068ce3fb4ef0a6c4cc6e6c15e0fee69
SUAT SATILMIŞ <suat.satilmis@ym.com.tr>
2022-10-05 11:35:21 +0300

    Created database with migrations

---
+++ src/demoProjects/rentACar/RentACar.Persistance/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentACar.Application.Services.Repositories;
+using RentACar.Persistance.Contexts;
+using RentACar.Persistance.Repositories;
+
+namespace RentACar.Persistance
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+                                                     options.UseSqlServer(
+                                                         configuration.GetConnectionString("ConnectionString")));
+
+            services.AddScoped<IBrandRepository, BrandRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/RentACar.Persistance/Repositories/BrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+using RentACar.Application.Services.Repositories;
+using RentACar.Domain.Entities;
+using RentACar.Persistance.Contexts;
+
+namespace RentACar.Persistance.Repositories
+{
+    public class BrandRepository: EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        public BrandRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
